d633ab90cf1f9793c05986544e9acde60ce5c687
Ramesh Krishnan <rameshkrishnan95@gmail.com>
2019-12-29 21:26:23 +0530

    Changed Verify Result

--- tests/calculator.js
+++ tests/calculator.js
@@ -2,10 +2,10 @@ let homepage = require('../pages/homepage');
 describe('demo calculator tests', function () {
     it('addition test', function () {
         homepage.get('http://juliemr.github.io/protractor-demo/')
-        homepage.enterFirstNumber('2');
+        homepage.enterFirstNumber('3');
         homepage.enterSecondNumber('3');
         homepage.clickGo();
-        homepage.verifyResult('5');
+        homepage.verifyResult('6');
         browser.sleep(2000);
     });
 });
